e1cab106ff09a6ff7fb1293e9c27a8e988efa8f3
Julidmz <104100843+Julidmz@users.noreply.github.com>
2022-06-04 00:53:17 -0300

    Add files via upload

---
+++ js/colordeletras1.js
@@ -0,0 +1,5 @@
+function colordeletras1() {
+    document.getElementById("subtitulo").style.Color = "white";
+    document.getElementById("ingredientes").style.Color = "white";
+    document.getElementById("procedimiento").style.Color = "white";
+}
---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+function cambiarfondo1() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("cabecera").style.backgroundColor = "black";
+    document.getElementById("principal").style.backgroundColor = "black";
+    document.getElementById("seccion").style.backgroundColor = "black";
+    document.getElementById("footer").style.backgroundColor = "black";
+    document.getElementById("subtitulo").style.color = "white";
+    document.getElementById("ingredientes").style.color = "white";
+    document.getElementById("preparacion").style.color = "white";
+}
+
+function cambiarfondo2() {
+    document.body.style.backgroundColor = "blue";
+    document.getElementById("cabecera").style.backgroundColor = "blue";
+    document.getElementById("principal").style.backgroundColor = "blue";
+    document.getElementById("seccion").style.backgroundColor = "blue";
+    document.getElementById("footer").style.backgroundColor = "blue";
+    document.getElementById("subtitulo").style.color = "white";
+    document.getElementById("ingredientes").style.color = "yellow";
+    document.getElementById("preparacion").style.color = "yellow";
+}
